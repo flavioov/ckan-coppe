be8e80286bc6bd9fd3a2ecd889508828bbb6f7d9
flavioov <oliveira.v653@gmail.com>
2020-12-14 08:41:31 -0300

    deleted folders

--- datapusher/stable/FILES/datapusher-entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-source "${CKAN_HOME}/default/bin/activate"
-
-exec python \
-    "${DATAPUSHER_HOME}/datapusher/main.py" \
-    "${DATAPUSHER_HOME}/deployment/datapusher_settings.py"
